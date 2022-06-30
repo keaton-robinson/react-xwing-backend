999ac65d13a56931f774434a9c3eefeb6242c9ac
Keaton Robinson <keatonr06@gmail.com>
2022-06-30 16:03:06 -0700

    Fixed api return & message format
    Also fixed a bug on the squads/delete endpoint. It was deleting the first
    squad it could find rather than the specified squad _id

--- routes/squads.js
+++ routes/squads.js
@@ -20,60 +20,51 @@ router.post('/', async (req, res) => {
 
     try {
         const savedData = await data.save();
-        res.status(200).json({success: true,  ...savedData});
+        res.status(200).json({success: true, savedSquad: savedData});
     } catch(error){
-        res.status(400).json({success:false,  errors: [error]});
+        res.status(400).json({success:false,  message: error.message});
     }
 })
 
 //Get all squads from specified faction belonging to authenticated user
 router.get('/:faction', async (req, res) => {
     try{
-        const data = await Squad.find({faction: req.params.faction, userId: req.jwt.sub });
-        res.status(200).json({success: true, ...data})
+        const foundSquads = await Squad.find({faction: req.params.faction, userId: req.jwt.sub });
+        res.status(200).json({success: true, squads: foundSquads})
     }
     catch(error){
-        res.status(500).json({success: false, errors: [error]})
+        res.status(500).json({success: false, message: error.message})
     }
 })
 
 //Get squad by id belonging to authenticated user
 router.get('/:id', async (req, res) => {
     try {
-        const data = await Squad.findOne({_id: req.params.id, userId: req.jwt.sub})  ;
-        res.json({success: true, ...data});
+        const foundSquad = await Squad.findOne({_id: req.params.id, userId: req.jwt.sub})  ;
+        res.json({success: true, squad: foundSquad});
     }
     catch(error){
-        res.status(500).json({success: false, errors: [error]});
+        res.status(500).json({success: false, message: error.message});
     }
 })
 
 
 //Update squad by id that belongs to authenticated user
 router.patch('/:id', async (req, res) => {
-    
-    
-    // const data = new Squad({
-    //     name: req.body.name,
-    //     points: req.body.points, //silly to have the front end send the points value...wouldn't do this in a real application
-    //     dateSaved: new Date(),
-    //     pilots: req.body.pilots
-    // });
-    
     try {
         const id = req.params.id;
         const userId = req.jwt.sub;
         const updatedData = req.body;
         const options = { new: true };
 
-        const result = await Squad.findOneAndUpdate(
+        const savedData = await Squad.findOneAndUpdate(
             { _id: id, userId: userId }, updatedData, options
         )
 
-        res.status(200).json({success:true, ...result})
+        res.status(200).json({success:true, savedSquad: savedData})
     }
     catch (error) {
-        res.status(400).json({ success: false, errors: [error] })
+        res.status(400).json({ success: false, message: error.message })
     }
 })
 
@@ -82,11 +73,11 @@ router.delete('/:id', async (req, res) => {
     try {
         const id = req.params.id;
         const userId = req.jwt.sub;
-        const data = await Squad.findOneAndDelete({id: id, userId: userId});
+        const data = await Squad.findOneAndDelete({_id: id, userId: userId});
         res.json({ success: true, message: `${data.name} has been deleted...`})
     }
     catch (error) {
-        res.status(400).json({ success: false, errors: [error] })
+        res.status(400).json({ success: false, message: error.message })
     }
 })
 
